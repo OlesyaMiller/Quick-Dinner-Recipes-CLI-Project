6c764d6985c15290cf929b4eb48157af951499fa
github username <github email address>
2020-02-13 04:49:00 -0400

    I think my app finally has all the functionality I wanted it to have and is bug free

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -74,16 +74,8 @@ class CommandLineInterface
         while input != "exit"
             input = gets.chomp 
             if (1..Recipe.all.length).include?(input.to_i)
-                selected_recipe = Recipe.all[input.to_i - 1]
-                puts "** #{selected_recipe.title} **"
-                puts "Author: #{selected_recipe.author}"
-                puts "Prep time: #{selected_recipe.prep_time}"
-                if selected_recipe.food_type != nil 
-                    puts "Diet: #{selected_recipe.food_type}"
-                    puts ""
-                end
-                puts ""
-                puts "#{selected_recipe.info}"
+                recipe = Recipe.all[input.to_i - 1]
+                recipe_information(recipe)
             elsif input.downcase == "exit"
                 exit_app 
             elsif input.downcase == "start over"
@@ -94,32 +86,35 @@ class CommandLineInterface
         end
     end
 
+    def recipe_information(recipe)
+        puts ""
+        puts "** #{recipe.title.upcase} **"
+        puts "Author: #{recipe.author}"
+        puts "Prep time: #{recipe.prep_time}"
+        if recipe.food_type != nil 
+            puts "Diet: #{recipe.food_type}"
+        end
+        puts ""
+        puts "#{recipe.info}"
+        puts ""
+    end
+
     def list_recipes_by_diet
         puts ""
         puts "Please enter the name of a diet from the following: gluten-free, healthy, low carb, vegetarian, paleo"
         puts ""
-            input = gets.chomp 
-            if input.downcase == "healthy" || input.downcase == "gluten-free" || input.downcase == "low carb" || input.downcase == "paleo" || input.downcase == "vegetarian"
-                @selected_recipes = Recipe.all.select do |recipe|
-                    recipe.food_type != nil && recipe.food_type.downcase.include?(input.downcase)
-                end
-                @selected_recipes.each.with_index(1) do |selected_recipe, index|
-                    puts "" 
-                    puts "#{index}. #{selected_recipe.title}"
-                end
-                # puts ""
-                # puts "To see recipe info, enter recipe number"
-                # puts "Enter a differnet diet name...."
-                recipe_by_diet_info
-           
-            elsif input.downcase == "exit"
-                exit_app
-            elsif input.downcase == "start over"
-                start_over
-            else
-                enter_correct_input
-                list_recipes_by_diet
-            end
+
+        input = gets.chomp 
+        if input.downcase == "healthy" || input.downcase == "gluten-free" || input.downcase == "low carb" || input.downcase == "paleo" || input.downcase == "vegetarian"
+            list_recipes_by_diet_helper_method(input)
+        elsif input.downcase == "exit"
+            exit_app
+        elsif input.downcase == "start over"
+            start_over
+        else
+            enter_correct_input
+            list_recipes_by_diet
+        end
     end
 
     def recipe_by_diet_info
@@ -128,29 +123,22 @@ class CommandLineInterface
         puts "To keep browsing, enter a differnet diet name"
         puts ""
         
-            input = gets.chomp 
-            
-            if (1..@selected_recipes.length).include?(input.to_i)
-                recipe = @selected_recipes[input.to_i - 1]
-                puts ""
-                puts "** #{recipe.title.upcase} **"
-                puts "Author: #{recipe.author}"
-                puts "Prep time: #{recipe.prep_time}"
-                puts "Diet: #{recipe.food_type}"
-                puts ""
-                puts "#{recipe.info}"
-                puts ""
-                last_level_method_for_diet
-            elsif input.downcase == "exit"
-                exit_app
-            elsif input.downcase == "start over"
-                start_over
-            elsif input.downcase == "healthy" || input.downcase == "gluten-free" || input.downcase == "low carb" || input.downcase == "paleo" || input.downcase == "vegetarian"
-                list_recipes_by_diet_helper_method(input)
-            else
-                enter_correct_input
-                recipe_by_diet_info
-            end
+        input = gets.chomp 
+        
+        if (1..@selected_recipes.length).include?(input.to_i)
+            recipe = @selected_recipes[input.to_i - 1]
+            recipe_information(recipe)
+            last_level_method_for_diet
+        elsif input.downcase == "exit"
+            exit_app
+        elsif input.downcase == "start over"
+            start_over
+        elsif input.downcase == "healthy" || input.downcase == "gluten-free" || input.downcase == "low carb" || input.downcase == "paleo" || input.downcase == "vegetarian"
+            list_recipes_by_diet_helper_method(input)
+        else
+            enter_correct_input
+            recipe_by_diet_info
+        end
     end
 
     def list_recipes_by_diet_helper_method(input)
@@ -169,83 +157,89 @@ class CommandLineInterface
         puts ""
         input = gets.chomp 
             
-            if (1..@selected_recipes.length).include?(input.to_i)
-                recipe = @selected_recipes[input.to_i - 1]
-                puts ""
-                puts "** #{recipe.title.upcase} **"
-                puts "Author: #{recipe.author}"
-                puts "Prep time: #{recipe.prep_time}"
-                puts "Diet: #{recipe.food_type}"
-                puts ""
-                puts "#{recipe.info}"
-                puts ""
-                last_level_method_for_diet
-            elsif input.downcase == "exit"
-                exit_app
-            elsif input.downcase == "start over"
-                start_over    
-            else
-                enter_correct_input
-                last_level_method_for_diet
-            end    
+        if (1..@selected_recipes.length).include?(input.to_i)
+            recipe = @selected_recipes[input.to_i - 1]
+            recipe_information(recipe)
+            last_level_method_for_diet
+        elsif input.downcase == "exit"
+            exit_app
+        elsif input.downcase == "start over"
+            start_over    
+        else
+            enter_correct_input
+            last_level_method_for_diet
+        end    
+    end
+
+    def last_level_method_for_prep_time
+        puts "To look up another recipe, enter recipe number" 
+        puts ""
+        input = gets.chomp 
+
+        if (1..@selected_recipes_by_prep_time.length).include?(input.to_i)
+            recipe = @selected_recipes_by_prep_time[input.to_i - 1]
+            recipe_information(recipe)
+            last_level_method_for_prep_time
+        elsif input.downcase == "exit"
+            exit_app
+        elsif input.downcase == "start over"
+            start_over
+        else
+            enter_correct_input
+            last_level_method_for_prep_time
+        end
     end
 
     def recipe_by_prep_time_info
         puts ""
         puts "To see recipe info, enter recipe number"
         puts "To keep browsing, enter how much time you have"
+        puts ""
         
-        input = nil 
-        while input != "exit"
-            input = gets.chomp 
-            
-            if (1..@selected_recipes_by_prep_time.length).include?(input.to_i)
-                recipe = @selected_recipes_by_prep_time[input.to_i - 1]
-                puts ""
-                puts "** #{recipe.title.upcase} **"
-                puts "Author: #{recipe.author}"
-                puts "Prep time: #{recipe.prep_time}"
-                if recipe.food_type != nil 
-                    puts "Diet: #{recipe.food_type}"
-                end
-                puts ""
-                puts "#{recipe.info}"
-                puts ""
-                puts "To exit the app, type 'exit'"
-                puts "To start over, type 'start over'"
-                puts ""
-            elsif input.downcase == "exit"
-                exit_app
-            elsif input.downcase == "start over"
-                start_over
-            else
-                enter_correct_input
-            end
+        input = gets.chomp 
+        
+        if (1..@selected_recipes_by_prep_time.length).include?(input.to_i) && !input.include?("min")
+            recipe = @selected_recipes_by_prep_time[input.to_i - 1]
+            recipe_information(recipe)
+            last_level_method_for_prep_time
+        elsif input.downcase == "exit"
+            exit_app
+        elsif input.downcase == "start over"
+            start_over
+        elsif input.split(" ")[0].to_i >= @prep_times.min && input.include?("min")
+            list_recipes_by_prep_time_helper_method(input)
+        else
+            enter_correct_input
+            recipe_by_prep_time_info
         end
     end
 
     def list_recipes_by_prep_time
-        prep_times = Recipe.all.map { |recipe| recipe.prep_time.split(" ")[0].to_i }
+        @prep_times = Recipe.all.map { |recipe| recipe.prep_time.split(" ")[0].to_i }
         # NO MAGIC NUMBERS!!!
-        puts "All the recipes take under #{prep_times.max} minutes to cook. The quickest recipe is #{prep_times.min} minutes. How much time do you have? (please enter a number followed by 'min' with a space in between, like this: 10 min)"
         puts ""
-        input = nil 
-        while input != "exit"
-            input = gets.chomp 
-            if input.to_i >= prep_times.min  
-                @selected_recipes_by_prep_time = Recipe.all.select { |recipe| recipe.prep_time.split(" ")[0].to_i <= input.to_i }
-                @selected_recipes_by_prep_time.each.with_index(1) do |selected_recipe, index|
-                    puts "#{index}. #{selected_recipe.title}"
-                    puts ""
-                end
-                recipe_by_prep_time_info
-            elsif input.downcase == "start over"
-                start_over
-            elsif input.downcase == "exit"
-                exit_app
-            else
-                enter_correct_input
-            end
+        puts "All the recipes take under #{@prep_times.max} minutes to cook. The quickest recipe is #{@prep_times.min} minutes. How much time do you have? (please enter a number followed by 'min' with a space in between, like this: 10 min)"
+        puts ""
+        input = gets.chomp 
+        if input.split(" ")[0].to_i >= @prep_times.min && input.include?("min")
+            list_recipes_by_prep_time_helper_method(input)
+            recipe_by_prep_time_info
+        elsif input.downcase == "start over"
+            start_over
+        elsif input.downcase == "exit"
+            exit_app
+        else
+            enter_correct_input
+            list_recipes_by_prep_time
         end
+    end
+
+    def list_recipes_by_prep_time_helper_method(input)
+        @selected_recipes_by_prep_time = Recipe.all.select { |recipe| recipe.prep_time.split(" ")[0].to_i <= input.split(" ")[0].to_i }
+        @selected_recipes_by_prep_time.each.with_index(1) do |selected_recipe, index|
+            puts "#{index}. #{selected_recipe.title}"
+            puts ""
+        end
+        recipe_by_prep_time_info
     end
 end
